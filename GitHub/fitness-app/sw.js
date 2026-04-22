cd658aad2cc05d87fb3180199eaa85c8d3c7320c
wykcnkqcdx-sketch <wykcnkqcdx@privaterelay.appleid.com>
2026-04-22 18:05:01 +0100

    Delete GitHub/fitness-app directory

--- GitHub/fitness-app/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting());
-
-self.addEventListener('activate', async () => {
-  const keys = await caches.keys();
-  await Promise.all(keys.map(k => caches.delete(k)));
-  await self.registration.unregister();
-  const clients = await self.clients.matchAll({ type: 'window' });
-  clients.forEach(c => c.navigate(c.url));
-});
